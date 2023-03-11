aab46619bc51f109bde83ed5d59872ce162d516c
SkipperSmahon <skipper_smahon@hotmail.com>
2023-03-11 22:48:32 +0800

    Updates

---
+++ precache-manifest.01653ea2ad125d87c9a73116c103a5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "013c92d5ac958c873f4b",
+    "url": "/static/css/main.fd179b21.chunk.css"
+  },
+  {
+    "revision": "013c92d5ac958c873f4b",
+    "url": "/static/js/main.013c92d5.chunk.js"
+  },
+  {
+    "revision": "6d5011ca78e5fe9fcc3a",
+    "url": "/static/js/1.6d5011ca.chunk.js"
+  },
+  {
+    "revision": "9b67f8eacc398732386a",
+    "url": "/static/js/2.9b67f8ea.chunk.js"
+  },
+  {
+    "revision": "ff52c985fcc41f0de2a7",
+    "url": "/static/js/runtime~main.ff52c985.js"
+  },
+  {
+    "revision": "f9db5822f03bc61d215d317855138986",
+    "url": "/index.html"
+  }
+];
